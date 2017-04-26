55dbca649d265d99eec83d8113bd2dd0ebc78837
klaud <klaudia.siedlarz@gmail.com>
2017-04-26 18:58:03 +0200

    LoginAndRegistration

---
+++ src/main/resources/static/registration.js
@@ -0,0 +1,38 @@
+
+/**
+ * Created by klaud on 26.04.2017.
+ */
+var app = angular.module('registration', []);
+
+app.controller('controller', function($scope,$http,$window,$log) {
+
+    $scope.Registartion=function(){
+        var data={
+            username: $scope.username,
+            password: $scope.password,
+            repeatPassword: $scope.repeatPassword,
+            email: $scope.email,
+            name: $scope.name
+
+        };
+        var config={ headers : {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+        }};
+        $http.post('http://localhost:8080/users/login', data)
+            .success(function (data, status, headers, config) {
+                $scope.result =data;
+                $scope.error=false;
+                var url = "http://" + $window.location.host + "/menu.html";
+                $log.log(url);
+                $window.location.href = url;
+            })
+            .error(function (data, status, header, config) {
+                /* $scope.result = "Data: " + data +
+                 "<hr />status: " + status +
+                 "<hr />headers: " + header +
+                 "<hr />config: " + config;*/
+                $scope.error=true;
+            });
+    };
+
+});
