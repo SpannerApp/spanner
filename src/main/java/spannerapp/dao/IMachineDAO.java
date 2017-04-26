7fc74b0b3f169c0dcab32ad7ebe806f19e8a70b8
mateuszstruzik <https://github.com/mateuszstruzik/ISM.git>
2017-04-27 01:02:56 +0200

    GET do tabeli MACHINE

---
+++ src/main/java/spannerapp/dao/IMachineDAO.java
@@ -0,0 +1,24 @@
+package spannerapp.dao;
+
+import spannerapp.model.Machine;
+
+import java.util.Collection;
+
+/**
+ * Created by MATI on 26.04.2017.
+ */
+public interface IMachineDAO {
+    Collection<Machine> getAllMachines();
+
+    Machine getMachineByID(int ID);
+
+   // User getUserByID(int ID);
+
+    /*void removeUserByID(int id);
+
+    void updateUserByID(User user);
+
+    void insertUser(User user);
+
+    boolean validateUser(User user);*/
+}
