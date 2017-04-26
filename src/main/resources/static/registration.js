a632c560c0044cc2ee8da1741479b42c1ff873af
klaud <klaudia.siedlarz@gmail.com>
2017-04-26 20:38:43 +0200

    FrontendConnection

--- src/main/resources/static/registration.js
+++ src/main/resources/static/registration.js
@@ -6,31 +6,30 @@ var app = angular.module('registration', []);
 
 app.controller('controller', function($scope,$http,$window,$log) {
 
-    $scope.Registartion=function(){
+    $scope.Register=function(){
         var data={
-            username: $scope.username,
-            password: $scope.password,
-            repeatPassword: $scope.repeatPassword,
-            email: $scope.email,
-            name: $scope.name
-
-        };
+            name: $scope.name,
+            surname: $scope.surname,
+            mail: $scope.mail,
+            login: $scope.login,
+            password: $scope.password
+                };
         var config={ headers : {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
         }};
-        $http.post('http://localhost:8080/users/login', data)
+        $http.post('http://localhost:8080/registration1', data)
             .success(function (data, status, headers, config) {
                 $scope.result =data;
                 $scope.error=false;
-                var url = "http://" + $window.location.host + "/menu.html";
+                var url = "http://" + $window.location.host + "/index.html";
                 $log.log(url);
                 $window.location.href = url;
             })
             .error(function (data, status, header, config) {
-                /* $scope.result = "Data: " + data +
+                $scope.result = "Data: " + data +
                  "<hr />status: " + status +
                  "<hr />headers: " + header +
-                 "<hr />config: " + config;*/
+                 "<hr />config: " + config;
                 $scope.error=true;
             });
     };
