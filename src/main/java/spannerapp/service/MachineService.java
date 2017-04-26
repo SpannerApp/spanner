4da90d2f239234a77c34e3a77b757c7f7c267609
mateuszstruzik <https://github.com/mateuszstruzik/ISM.git>
2017-04-27 01:03:32 +0200

    GET do tabeli MACHINE

---
+++ src/main/java/spannerapp/service/MachineService.java
@@ -0,0 +1,28 @@
+package spannerapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import spannerapp.dao.IMachineDAO;
+import spannerapp.model.Machine;
+
+import java.util.Collection;
+
+/**
+ * Created by MATI on 26.04.2017.
+ */
+@Service
+public class MachineService {
+
+    @Autowired
+    @Qualifier("sqlserver2")
+    private IMachineDAO machineDAO;
+
+    public Collection<Machine> getAllMachines() {
+        return machineDAO.getAllMachines();
+    }
+
+    public Machine getMachineByID(int ID) {
+        return this.machineDAO.getMachineByID(ID);
+    }
+}
