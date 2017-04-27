2e663f98378402f0c859869246298e269c92e83c
gabfur <209143@student.pwr.edu.pl>
2017-04-27 14:28:40 +0200

    search angular

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -2,28 +2,30 @@ var app = angular.module('login', []);
 
 app.controller('controller', function($scope,$http,$window,$log) {
 
-      $scope.Login=function(){
-        var data={
+    $scope.Login = function () {
+        var data = {
             username: $scope.username,
             password: $scope.password
         };
-        var config={ headers : {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-        }};
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
         $http.post('http://localhost:8080/users/login', data)
             .success(function (data, status, headers, config) {
-                $scope.result =data;
-                $scope.error=false;
+                $scope.result = data;
+                $scope.error = false;
                 var url = "http://" + $window.location.host + "/menu.html";
                 $log.log(url);
                 $window.location.href = url;
-              })
+            })
             .error(function (data, status, header, config) {
-               /* $scope.result = "Data: " + data +
-                    "<hr />status: " + status +
-                    "<hr />headers: " + header +
-                    "<hr />config: " + config;*/
-               $scope.error=true;
+                /* $scope.result = "Data: " + data +
+                 "<hr />status: " + status +
+                 "<hr />headers: " + header +
+                 "<hr />config: " + config;*/
+                $scope.error = true;
             });
     };
 
--- src/main/resources/static/search.js
+++ src/main/resources/static/search.js
@@ -1,33 +1,28 @@
-var app = angular.module('registration', []);
+var app = angular.module('search', []);
 
 app.controller('controller', function($scope,$http,$window,$log) {
 
-    $scope.Register=function(){
+    $scope.Login=function(){
         var data={
-            name: $scope.name,
-            surname: $scope.surname,
-            mail: $scope.mail,
-            login: $scope.login,
-            password: $scope.password
-                };
+            code: $scope.code
+        };
         var config={ headers : {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
         }};
-        $http.post('http://localhost:8080/registration', data)
+        $http.post('http://localhost:8080/findByCode', data)
             .success(function (data, status, headers, config) {
                 $scope.result =data;
                 $scope.error=false;
-                var url = "http://" + $window.location.host + "/index.html";
+                var url = "http://" + $window.location.host + "/information.html";
                 $log.log(url);
                 $window.location.href = url;
             })
             .error(function (data, status, header, config) {
-                $scope.result = "Data: " + data +
+                /* $scope.result = "Data: " + data +
                  "<hr />status: " + status +
                  "<hr />headers: " + header +
-                 "<hr />config: " + config;
+                 "<hr />config: " + config;*/
                 $scope.error=true;
             });
     };
-
 });
