2e44abd99b0c554801e752e5462477e12cd088ed
spaceone <develop@spaceone.pl>
2017-06-03 18:25:25 +0200

    fix user session Pawel
    Signed-off-by: spaceone <develop@spaceone.pl>

--- src/main/java/spannerapp/config/SecurityConfig.java
+++ src/main/java/spannerapp/config/SecurityConfig.java
@@ -12,26 +12,28 @@ import javax.sql.DataSource;
 /**
  * Created by Siekacz on 03.06.2017.
  */
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    DataSource dataSource;
+//
+//    private static final String CHECK_USER_BY_USERNAME = "SELECT Username, Password From AuthUser where Username=?";
+//    private static final String CHECK_AUTH_BY_USERNAME_ROLES = "SELECT au.Login, RoleName FROM AuthUser au join UserRoles ur on au.UserID = ur.UserID join Roles r on ur.RoleID = r.RoleID WHERE au.Login=?";
+//
+//    @Autowired
+//    public void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception{
+//        auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(CHECK_USER_BY_USERNAME).authoritiesByUsernameQuery(CHECK_AUTH_BY_USERNAME_ROLES);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic().and().authorizeRequests().antMatchers("/**").access("hasRole('ADMIN')").
+//                anyRequest().fullyAuthenticated().and().formLogin().loginPage("/index.html").usernameParameter("username").passwordParameter("password").
+//                and().exceptionHandling().accessDeniedPage("/reqister.html").and().csrf();
+//    }
+//
+//}
 
-    @Autowired
-    DataSource dataSource;
-
-    private static final String CHECK_USER_BY_USERNAME = "SELECT Username, Password From AuthUser where Username=?";
-    private static final String CHECK_AUTH_BY_USERNAME_ROLES = "SELECT au.Login, RoleName FROM AuthUser au join UserRoles ur on au.UserID = ur.UserID join Roles r on ur.RoleID = r.RoleID WHERE au.Login=?";
-
-    @Autowired
-    public void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception{
-        auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(CHECK_USER_BY_USERNAME).authoritiesByUsernameQuery(CHECK_AUTH_BY_USERNAME_ROLES);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests().antMatchers("/**").access("hasRole('ADMIN')").
-                anyRequest().fullyAuthenticated().and().formLogin().loginPage("/index.html").usernameParameter("username").passwordParameter("password").
-                and().exceptionHandling().accessDeniedPage("/reqister.html").and().csrf();
-    }
-
-}
+public class SecurityConfig{}
