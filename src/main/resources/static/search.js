0c776b44a8af978f7ba5a47502586b73275a4315
Gabriela <gabrielaela144@gmail.com>
2017-06-04 14:51:53 +0200

    Founded product reparation and raport

--- src/main/resources/static/search.js
+++ src/main/resources/static/search.js
@@ -68,7 +68,7 @@ app.controller('InfoController', function($scope,$http,$window) {
 
                 $scope.error=true;
             });
-     };
+     });
 
 
     $scope.reportIssue=function(){
@@ -98,33 +98,7 @@ app.controller('InfoController', function($scope,$http,$window) {
     };
 
 });
-app.service('srvShareData', function($window) {
-    var KEY = 'App.SelectedValue';
 
-    var addData = function(newObj) {
-        var mydata = $window.sessionStorage.getItem(KEY);
-        if (mydata) {
-            mydata = JSON.parse(mydata);
-        } else {
-            mydata = [];
-        }
-        mydata.push(newObj);
-        $window.sessionStorage.setItem(KEY, JSON.stringify(mydata));
-    };
-
-    var getData = function(){
-        var mydata = $window.sessionStorage.getItem(KEY);
-        if (mydata) {
-            mydata = JSON.parse(mydata);
-        }
-        return mydata || [];
-    };
-
-    return {
-        addData: addData,
-        getData: getData
-    };
-});
 // app.service('srvShareData', function($window) {
 //     var KEY = 'App.SelectedValue';
 //
