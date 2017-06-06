950cd104f022babcf5e87eee19bc5768ebcc4c07
spaceone <develop@spaceone.pl>
2017-06-06 21:55:56 +0200

    create user procedure Pawel
    Signed-off-by: spaceone <develop@spaceone.pl>

---
+++ src/main/resources/db/migration/V0_1_1_Create_user_procedure.sql
@@ -0,0 +1,24 @@
+IF OBJECT_ID('dbo.SP_CREATE_USER') IS NOT NULL
+    DROP PROCEDURE SP_CREATE_USER
+GO
+
+CREATE PROCEDURE SP_CREATE_USER
+
+    @Login NVARCHAR(50),
+    @Password NVARCHAR(50),
+    @EmployeeID INT,
+    @RoleID INT,
+    @UserID INT
+AS
+  BEGIN
+
+    IF EXISTS(SELECT * FROM AuthUser WHERE Login = @Login)
+      BEGIN
+        SET @UserID = -1
+        RETURN @UserID
+      END
+
+    INSERT INTO AuthUser (Login, Password, EmployeeID, RoleID)  VALUES (@Login, @Password, @EmployeeID, @RoleID)
+    SET @UserID = @@IDENTITY
+  END
+GO
