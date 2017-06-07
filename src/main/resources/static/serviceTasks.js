f260609c3eec17282cc88b790ff5921e95157d67
Gabriela <gabrielaela144@gmail.com>
2017-06-07 19:06:07 +0200

    Add Ready button Sort

--- src/main/resources/static/serviceTasks.js
+++ src/main/resources/static/serviceTasks.js
@@ -7,33 +7,9 @@ app.controller("TasksController", function($scope, $http,$window) {
             $scope.issues= result.data;
         });
 
-    $scope.Reject=function(id){
+    $scope.ChangeStatus=function(id,status){
         var data={
-            issueStatus: "REJECTED",
-            id: parseInt(id)
-        };
-        var config={ headers : {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-        }};
-        $http.put('http://localhost:8080//reportedIssues/changeStatus', data)
-            .success(function (data, status, headers, config) {
-                $scope.result =data;
-                $scope.taskStatus="REJECTED";
-                $window.location.reload();
-            })
-            .error(function (data, status, header, config) {
-                // $scope.result = "Data: " + data +
-                //     "<hr />status: " + status +
-                //     "<hr />headers: " + header +
-                //     "<hr />config: " + config;
-                $scope.error=true;
-                $scope.success=false;
-            });
-
-    };
-    $scope.Accept=function(id){
-        var data={
-            issueStatus: "ACCEPTED",
+            issueStatus: status,
             id: parseInt(id)
         };
         var config={ headers : {
